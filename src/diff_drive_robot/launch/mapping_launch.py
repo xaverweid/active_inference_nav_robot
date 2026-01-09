67ab2318bef781bae8cf67ad21a5d4a3c5982468
Your Name <your-email@example.com>
2026-01-09 14:31:34 +0000

    removed EKF file and capabilities.

--- src/diff_drive_robot/launch/mapping_launch.py
+++ src/diff_drive_robot/launch/mapping_launch.py
@@ -3,7 +3,7 @@ from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
 from launch.conditions import IfCondition
 from launch.launch_description_sources import PythonLaunchDescriptionSource
-from launch.substitutions import LaunchConfiguration, PathJoinSubstitution, Command
+from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
 from launch_ros.actions import Node
 from ament_index_python.packages import get_package_share_directory
 
--- src/diff_drive_robot/launch/robot_launch.py
+++ src/diff_drive_robot/launch/robot_launch.py
@@ -3,7 +3,7 @@ from launch_ros.actions import Node
 from launch import LaunchDescription
 from launch.conditions import IfCondition
 from ament_index_python.packages import get_package_share_directory
-from launch.substitutions import LaunchConfiguration, EnvironmentVariable
+from launch.substitutions import LaunchConfiguration
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.actions import IncludeLaunchDescription, DeclareLaunchArgument, GroupAction, SetEnvironmentVariable, ExecuteProcess, TimerAction
 
