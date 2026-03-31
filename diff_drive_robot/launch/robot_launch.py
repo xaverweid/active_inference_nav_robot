df50110fb4a9b16bc9aebc8e3e76ca2691d961e8
Your Name <your-email@example.com>
2026-03-31 16:05:16 +0100

    added new h-map with starting poses. experimental preparation

--- diff_drive_robot/launch/robot_launch.py
+++ diff_drive_robot/launch/robot_launch.py
@@ -9,7 +9,7 @@ from launch.actions import IncludeLaunchDescription, DeclareLaunchArgument, Grou
 
 def generate_launch_description():
 
-    x_pose = LaunchConfiguration('x_pose', default='-2.0')
+    x_pose = LaunchConfiguration('x_pose', default='-1.0')
     y_pose = LaunchConfiguration('y_pose', default='0.0')
     yaw_pose = LaunchConfiguration('yaw_pose', default='0.0')
 
@@ -158,7 +158,7 @@ def generate_launch_description():
         rsp,
         gazebo_server,
         # disable GUI for run_experiment experimental run script
-        gazebo_client, 
+        # gazebo_client, 
         ros_gz_bridge,
         spawn_diff_bot,
         starting_pose_publisher,
